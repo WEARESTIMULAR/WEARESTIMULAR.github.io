56360d21d6135dcc8c51b44113c727d91ee9c2a5
WEARESTIMULAR <stimular.github@gmail.com>
2025-04-02 23:21:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/WEARESTIMULAR.github.io/', // Change this to match your repo name
+  build: {
+    outDir: 'dist',
+  }
 });
