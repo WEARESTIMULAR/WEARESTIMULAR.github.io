a815cb32aaff678be67cb2834f7f153357976c2c
WEARESTIMULAR <stimular.github@gmail.com>
2025-04-03 17:55:15 +0200

    delete everything

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/WEARESTIMULAR.github.io/', // Change this to match your repo name
-  build: {
-    outDir: 'dist',
-  }
-});
